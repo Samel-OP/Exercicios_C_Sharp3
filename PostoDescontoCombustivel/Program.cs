56d779a7ea3798dea51759d77435aa5da1cc25a3
Samel <samueldeoliveirapereira1@gmail.com>
2021-05-11 19:13:19 -0300

    Exercício_2

--- PostoDescontoCombustivel/Program.cs
+++ PostoDescontoCombustivel/Program.cs
@@ -6,13 +6,13 @@ namespace PostoDescontoCombustivel
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Digite qual tipo de combustível foi comprado: (A) Álcool / (G) Gasolina");
+            Console.WriteLine("Qual tipo de combustível você deseja abastecer? (A) Álcool / (G) Gasolina");
             string combustivel = Console.ReadLine().ToUpper();
 
             switch (combustivel)
             {
                 case "A":
-                    Console.WriteLine("Quantos litros foram comprados?");
+                    Console.WriteLine("Quantos litros você deseja?");
                     float litrosA = float.Parse(Console.ReadLine());
                     double valorAlcool = litrosA * 4.90;
 
@@ -20,18 +20,20 @@ namespace PostoDescontoCombustivel
                     {
                         double desconto = valorAlcool * 0.03;
                         double total = valorAlcool - desconto;
+                        Console.ForegroundColor = ConsoleColor.Blue;
                         Console.WriteLine($"O valor a ser pago é: R$ {total}");
                     }
                     else if (litrosA > 20)
                     {
                         double desconto = valorAlcool * 0.05;
                         double total = valorAlcool - desconto;
+                        Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine($"O valor a ser pago é: R$ {total}");
                     }
                     break;
 
                 case "G":
-                    Console.WriteLine("Quantos litros foram comprados?");
+                    Console.WriteLine("Quantos litros você deseja?");
                     float litrosB = float.Parse(Console.ReadLine());
                     double valorGasolina = litrosB * 5.30;
 
@@ -39,17 +41,20 @@ namespace PostoDescontoCombustivel
                     {
                         double desconto = valorGasolina * 0.03;
                         double total = valorGasolina - desconto;
+                        Console.ForegroundColor = ConsoleColor.Blue;
                         Console.WriteLine($"O valor a ser pago é: R$ {total}");
                     }
                     else if (litrosB > 20)
                     {
                         double desconto = valorGasolina * 0.05;
                         double total = valorGasolina - desconto;
+                        Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine($"O valor a ser pago é: R$ {total}");
                     }
                     break;
-                    
+
                 default:
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Combustível inválido!");
                     break;
             }
